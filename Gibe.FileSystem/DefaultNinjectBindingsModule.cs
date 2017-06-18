5402f87b94ed3910ec1c9f6b6ae9b84eda587018
Steve Temple <steve@gibedigital.com>
2017-06-18 22:28:01 +0100

    Add gitignore and ninject bindings

---
+++ Gibe.FileSystem/DefaultNinjectBindingsModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject.Modules;
+
+namespace Gibe.FileSystem
+{
+	public class DefaultNinjectBindingsModule : NinjectModule
+	{
+		public override void Load()
+		{
+			Bind<IDirectoryService>().To<DirectoryService>().InSingletonScope();
+			Bind<IFileService>().To<FileService>();
+			Bind<IPaths>().To<Paths>();
+		}
+	}
+}
